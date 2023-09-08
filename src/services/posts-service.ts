4243cc837159d83e6eed4209b00ced14b531cb5e
IronPlus <mav.plus@gmail.com>
2023-09-08 19:11:06 +0300

    use basic auth with session storage

--- src/services/posts-service.ts
+++ src/services/posts-service.ts
@@ -1,12 +1,13 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import config from "../config";
 import { Post } from "../models/post";
+import createBasicAuthHeaderFromSession from "./auth-service";
 
 export default function getPostsByPageNum(page: number): Promise<Post[]> {
   return axios
     .get(`${config.baseUrl}/posts/${page}`, {
       headers: {
-        Authorization: config.authorizationHeader,
+        Authorization: createBasicAuthHeaderFromSession(),
       },
     })
     .then((res: AxiosResponse) => {
