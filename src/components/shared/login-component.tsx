9ecc7db44c6153881617da043ac31212a478ee63
IronPlus <mav.plus@gmail.com>
2023-09-08 16:36:10 +0300

    Add login component

--- src/components/shared/login-component.tsx
+++ src/components/shared/login-component.tsx
@@ -6,22 +6,12 @@ import {
   CardHeader,
   CardContent,
 } from "@mui/material";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Login: React.FC = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  sessionStorage.removeItem("username");
-  sessionStorage.removeItem("password");
-
+export default function Login() {
   const navigate = useNavigate();
 
   const handleNavigation = () => {
-    sessionStorage.setItem("username", username);
-    sessionStorage.setItem("password", password);
-
     navigate("/posts");
   };
 
@@ -29,22 +19,12 @@ const Login: React.FC = () => {
     <div>
       <Container maxWidth="sm" className="login-component">
         <Card elevation={8} className="login-component__card">
-          <CardHeader title="Welcome back, Log in!"></CardHeader>
+          <CardHeader title="Log in or sign up"></CardHeader>
           <CardContent>
-            <TextField
-              label="Username"
-              variant="filled"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
+            <TextField label="Username" variant="filled" />
             <br />
             <br />
-            <TextField
-              label="Password"
-              variant="filled"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <TextField label="Password" variant="filled" />
             <br />
             <br />
             <Button
@@ -52,13 +32,11 @@ const Login: React.FC = () => {
               variant="contained"
               onClick={handleNavigation}
             >
-              Log in
+              Login
             </Button>
           </CardContent>
         </Card>
       </Container>
     </div>
   );
-};
-
-export default Login;
+}
